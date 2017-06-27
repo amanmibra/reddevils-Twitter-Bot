fbe7812debaf3622b78dab8a045636d3d92a089f
amanmibra <amanmibra@gmail.com>
2017-06-27 15:38:57 -0400

    Posting "Hello World"

--- src/bot.js
+++ src/bot.js
@@ -5,20 +5,16 @@ var config = require('./config');
 
 var T = new Twit(config);
 
-var search = {
-  q: 'Manchester United',
-  count: 11
-};
+var tweet = {
+  status: "Hello World! I am Aman's bot. Follow him @amanmibra!"
+}
 
-T.get('search/tweets', search, getTweets);
+T.post('statuses/update', tweet, tweeted);
 
-
-function getTweets(err, data, response) {
-  var statuses = data.statuses;
-  for(var i = 0; i < statuses.length; i++){
-    var tweet = statuses[i].text;
-    console.log((i+1) + ") " + tweet);
-    if(i == statuses.length - 1) break;
-    console.log("------------")
+function tweeted(err, data, response) {
+  if(err){
+    console.log("Not working")
+  } else{
+    console.log('It worked!')
   }
 }
