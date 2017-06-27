a73c87ef8e2618e7d21be5f4dc77363d8d88bf74
amanmibra <amanmibra@gmail.com>
2017-06-27 17:13:19 -0400

    Updated atom packages

--- src/bot.js
+++ src/bot.js
@@ -1,36 +1,34 @@
 console.log('The bot is working');
-
 var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
 var stream = T.stream('user');
 
-stream.on('follow', followed)
+stream.on('follow', followed);
 
-
-function followed(event){
+function followed(event) {
   var name = event.source.name;
   var handle = event.source.screen_name;
 
-  console.log("I have been followed!");
+  console.log('I have been followed!');
 
-  var followTweet = "Thanks for following me @" + handle + "!";
+  var followTweet = 'Thanks for following me @' + handle + '!';
   tweetIt(followTweet);
 }
 
 function tweetIt(input) {
   var tweet = {
-    status: input
-  }
+    status: input,
+  };
 
   T.post('statuses/update', tweet, tweeted);
 
   function tweeted(err, data, response) {
     if (err) {
-      console.log("Not working")
+      console.log('Not working');
     } else {
-      console.log('It worked!')
+      console.log('It worked!');
     }
   }
 }
