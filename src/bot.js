f25bc8a1073c657ad2159683c621f863d62fb108
amanmibra <amanmibra@gmail.com>
2017-06-27 15:58:33 -0400

    Added follow tweet feature

--- src/bot.js
+++ src/bot.js
@@ -4,17 +4,33 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
+var stream = T.stream('user');
 
-var tweet = {
-  status: "Hello World! I am Aman's bot. Follow him @amanmibra!"
+stream.on('follow', followed)
+
+
+function followed(event){
+  var name = event.source.name;
+  var handle = event.source.screen_name;
+
+  console.log("I have been followed!");
+
+  var followTweet = "Thanks for following me @" + handle + "!";
+  tweetIt(followTweet);
 }
 
-T.post('statuses/update', tweet, tweeted);
+function tweetIt(input) {
+  var tweet = {
+    status: input
+  }
 
-function tweeted(err, data, response) {
-  if(err){
-    console.log("Not working")
-  } else{
-    console.log('It worked!')
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("Not working")
+    } else {
+      console.log('It worked!')
+    }
   }
 }
