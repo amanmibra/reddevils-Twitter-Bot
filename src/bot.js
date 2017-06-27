dd71e9ec1339c2cc9d3f716de4a5cc6b45a8f0a1
amanmibra <amanmibra@gmail.com>
2017-06-27 19:49:01 -0400

    Added ability to post media

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,34 @@
 console.log('The bot is working');
 var Twit = require('twit');
 var config = require('./config');
+var fs = require('fs');
 
 var T = new Twit(config);
 var stream = T.stream('user');
 
 stream.on('follow', followed);
+var filename = "../images/gort.jpg";
+var msg = "This picture is so good, I will post it twice! #Gort";
+makeMediaPost(filename, msg);
+
+function makeMediaPost(filename, msg){
+  var params = {
+    encoding: "base64",
+  };
+  var b64 = fs.readFileSync(filename, params);
+
+  T.post('media/upload', { media_data: b64 }, uploaded);
+
+  function uploaded(err, data, response){
+    var id = data.media_id_string;
+    var media_tweet = {
+      status: msg,
+      media_ids: [id],
+    }
+
+    T.post('statuses/update', media_tweet, tweeted);
+  }
+}
 
 function followed(event) {
   var name = event.source.name;
@@ -23,12 +46,12 @@ function tweetIt(input) {
   };
 
   T.post('statuses/update', tweet, tweeted);
+}
 
-  function tweeted(err, data, response) {
-    if (err) {
-      console.log('Not working');
-    } else {
-      console.log('It worked!');
-    }
+function tweeted(err, data, response) {
+  if (err) {
+    console.log('Not working');
+  } else {
+    console.log('It worked!');
   }
 }
