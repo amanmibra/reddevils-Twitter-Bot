8b337d4a9e46ca78246aa61fa8e88e6ac39cb763
amanmibra <amanmibra@gmail.com>
2017-06-27 14:53:13 -0400

    Implementing post Twit feature

--- src/bot.js
+++ src/bot.js
@@ -4,3 +4,21 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
+
+var search = {
+  q: 'Manchester United',
+  count: 11
+};
+
+T.get('search/tweets', search, getTweets);
+
+
+function getTweets(err, data, response) {
+  var statuses = data.statuses;
+  for(var i = 0; i < statuses.length; i++){
+    var tweet = statuses[i].text;
+    console.log((i+1) + ") " + tweet);
+    if(i == statuses.length - 1) break;
+    console.log("------------")
+  }
+}
