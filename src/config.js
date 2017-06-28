2f1673bedee38d33924232f70f6d57625332a750
amanmibra <amanmibra@gmail.com>
2017-06-28 16:12:45 -0400

    Improved bot

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'eix6i5grSEDiH3WUjWOISzX7s',
-  consumer_secret:      'tzk723tPwVvumr0dK0kC9fLKmMvbHskFngt6lZKTOLeJb7baEd',
+  consumer_key:         'vFH0UWDVMKRmN1hIiBhmlM7U9',
+  consumer_secret:      'UQiKsAo5Vavf9TMOvOlbZgJmjefOCYnQ5rM3QZu1C7wwi91lGE',
   access_token:         '879762040704913409-dPFu4WP6gPIqyjraJis5sT4V9n9wD97',
   access_token_secret:  'aOzPCo5DEw6Gi78KLLywG8MydZHxjv5aKjU9YRibEqTrm',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
