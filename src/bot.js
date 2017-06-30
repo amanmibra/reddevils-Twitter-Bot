2ee04de79fa10356e4a7c25d3aa7fa912359253b
amanmibra <amanmibra@gmail.com>
2017-06-29 22:56:48 -0400

    Fixed title

--- src/bot.js
+++ src/bot.js
@@ -163,6 +163,8 @@ function checkDuplicate(tweet){
 function titleChecker(title){
   if(title.length > 50){
     title = title.substring(0, 50);
+  } else {
+    return title;
   }
   return title + "...";
 }
